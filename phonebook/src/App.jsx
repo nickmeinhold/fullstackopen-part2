971b0501b48d0dec03df4bec541c1278f361f6a0
Nick Meinhold <nick@enspyr.co>
2025-11-04 22:11:07 +1100

    Completet 2.12

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -32,6 +32,14 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat({ name: newName, number: newNumber }));
+      axios
+        .post("http://localhost:3001/persons", {
+          name: newName,
+          number: newNumber,
+        })
+        .then((response) => {
+          console.log(response);
+        });
     }
 
     setNewName("");
